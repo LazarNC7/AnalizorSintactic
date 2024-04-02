da6598653a78425160f8b642a500f40822a8569d
LazarNC7 <109949877+LazarNC7@users.noreply.github.com>
2024-04-02 13:03:38 +0300

    Update lexer.c

--- lexer.c
+++ lexer.c
@@ -1,404 +1,236 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-#include <regex.h>
+#include <stdlib.h>
+
 #include "lexer.h"
 #include "utils.h"
-#include <stdlib.h>
-#include <string.h>
 
-Token *tokens; // single linked list of tokens
-Token *lastTk; // the last token in list
+char *array[] = {"ID"
+		 // keywords
+		 ,"TYPE_CHAR", "TYPE_DOUBLE", "ELSE", "IF", "TYPE_INT",
+		 "RETURN", "STRUCT", "VOID", "WHILE",
+		 // constants
+		 "INT", "DOUBLE", "CHAR", "STRING"
+		 // delimiters
+		 ,"COMMA","END", "SEMICOLON", "LPAR", "RPAR",
+		 "LBRACKET", "RBRACKET", "LACC", "RACC"
+		 // operators
+		 ,"ASSIGN","EQUAL", "ADD", "SUB", "MUL", "DIV", "DOT", "AND",
+		 "OR", "NOT", "NOTEQ", "LESS", "LESSEQ", "GREATER", "GREATEREQ"};
 
-int line = 1; // the current line in the input file
+Token *tokens;	// single linked list of tokens
+Token *lastTk;		// the last token in list
+
+int line=1;		// the current line in the input file
 
 // adds a token to the end of the tokens list and returns it
 // sets its code and line
-Token *addTkText(int code, char* text)
-{
-    Token *tk = safeAlloc(sizeof(Token));
-    tk->code = code;
-    tk->line = line;
-    tk->next = NULL;
-    if (lastTk)
-    {
-        lastTk->next = tk;
-    }
-    else
-    {
-        tokens = tk;
-    }
-    lastTk = tk;
-
-    switch(code){
-    case INT: {
-      tk->i=atoi(text);
-      break;
-    }
-    case DOUBLE:{
-      tk->d=atof(text);
-      break;
-    }
-      case CHAR:{
-      tk->c=*text;
-      break;
-    }
-    case STRING:{
-      tk->text=text;
-      break;
-    }
-      case ID:{
-      tk->text=text;
-      break;
-    }
-    default:{
-      tk->text=NULL;
-    }
-    }
-    return tk;
+Token *addTk(int code){
+  Token *tk=safeAlloc(sizeof(Token));
+  tk->code=code;
+  tk->line=line;
+  tk->next=NULL;
+  if(lastTk){
+    lastTk->next=tk;
+  }else{
+    tokens=tk;
+  }
+  lastTk=tk;
+  return tk;
 }
 
-Token *addTk(int code)
-{
-    Token *tk = safeAlloc(sizeof(Token));
-    tk->code = code;
-    tk->line = line;
-    tk->next = NULL;
-    if (lastTk)
+char *extract(const char *begin,const char *end){
+  size_t size = end-begin;
+  char *buffer = (char*)malloc((size+1)*sizeof(char));
+  if(!buffer){
+    printf("nu se poate aloca sirul\n");
+    exit(-1);
+  }
+  for(int i=0; i<size; i++)
     {
-        lastTk->next = tk;
+      buffer[i]=begin[i];
     }
-    else
-    {
-        tokens = tk;
-    }
-    lastTk = tk;
-    return tk;
+  buffer[size]='\0';
+  return buffer;
 }
 
-char *extract(const char *begin, const char *end)
-{
-    int len = end - begin;
-    char *text = malloc(len + 1);
-    if (text == NULL)
-    {
-        err("Memory allocation failed in extract");
-    }
-    strncpy(text, begin, len);
-    text[len] = '\0';
-    return text;
-}
-
-void regexCheck(const char *pch, char* text, Token* tk)
-{
-    regex_t regex;
-    int reti;
-
-    const char *int_pattern = "[0-9]+";
-    const char *double_pattern = "[0-9]+('.'[0-9]+([eE][+-]?[0-9]+)?|('.'[0-9]+)?[eE][+-]?[0-9]+)";
-    const char *char_pattern = "'.'";
-    const char *string_pattern = "\"[^\"]*\"";
-
-    reti = regcomp(&regex, int_pattern, REG_EXTENDED);
-    if (reti)
-    {
-        fprintf(stderr, "Could not compile regex for INT\n");
-        exit(EXIT_FAILURE);
-    }
-
-    reti = regexec(&regex, pch, 0, NULL, 0);
-    if (!reti)
-    {
-      tk=addTkText(INT, text);
-    }
-    else
-    {
-
-        reti = regcomp(&regex, double_pattern, REG_EXTENDED);
-        if (reti)
-        {
-            fprintf(stderr, "Could not compile regex for DOUBLE\n");
-            exit(EXIT_FAILURE);
-        }
-        reti = regexec(&regex, pch, 0, NULL, 0);
-        if (!reti)
-        {
-	  tk=addTkText(DOUBLE, text);
-        }
-        else
-        {
-
-            reti = regcomp(&regex, char_pattern, REG_EXTENDED);
-            if (reti)
-            {
-                fprintf(stderr, "Could not compile regex for CHAR\n");
-                exit(EXIT_FAILURE);
-            }
-            reti = regexec(&regex, pch, 0, NULL, 0);
-            if (!reti)
-            {
-	      tk=addTkText(CHAR, text);
-            }
-            else
-            {
-
-                reti = regcomp(&regex, string_pattern, REG_EXTENDED);
-                if (reti)
-                {
-                    fprintf(stderr, "Could not compile regex for STRING\n");
-                    exit(EXIT_FAILURE);
-                }
-                reti = regexec(&regex, pch, 0, NULL, 0);
-                if (!reti)
-                {
-		  tk=addTkText(STRING, text);
-                }
-                else
-                {
-                    err("invalid char: %c (%d)", *pch, *pch);
-                }
-            }
-        }
-    }
-}
-Token *tokenize(const char *pch)
-{
-    // printf("1\n");
-    const char *start;
-    Token *tk;
-    for (;;)
-    {
-        // printf("%d\n", i++);
-        switch (*pch)
-        {
-        case ' ':
-        case '\t':
-            pch++;
-            break;
-        case '\r': // handles different kinds of newlines (Windows: \r\n, Linux: \n, MacOS, OS X: \r or \n)
-            if (pch[1] == '\n')
-                pch++;
-            // fallthrough to \n
-        case '\n':
-            line++;
-            pch++;
-            break;
-        case '\0':
-        {
-            addTk(END);
-            return tokens;
-        }
-        case EOF:
-        {
-            addTk(END);
-            return tokens;
-        }
-        case ',':
-            addTk(COMMA);
-            pch++;
-            break;
-        case ';':
-            addTk(SEMICOLON);
-            pch++;
-            break;
-        case '(':
-            addTk(LPAR);
-            pch++;
-            break;
-        case ')':
-            addTk(RPAR);
-            pch++;
-            break;
-        case '[':
-            addTk(LBRACKET);
-            pch++;
-            break;
-        case ']':
-            addTk(RBRACKET);
-            pch++;
-            break;
-        case '{':
-            addTk(LACC);
-            pch++;
-            break;
-        case '}':
-            addTk(RACC);
-            pch++;
-            break;
-        case '=':
-            if (pch[1] == '=')
-            {
-                addTk(EQUAL);
-                pch += 2;
-            }
-            else
-            {
-                addTk(ASSIGN);
-                pch++;
-            }
-            break;
-        case '+':
-        {
-            addTk(ADD);
-            pch++;
-            break;
-        }
-        case '-':
-        {
-            addTk(SUB);
-            pch++;
-            break;
-        }
-        case '*':
-        {
-            addTk(SUB);
-            pch++;
-            break;
-        }
-        case '/':
-        {
-            if (pch[1] == '/')
-            {
-
-                while (*pch != '\n')
-                    pch++;
-            }
-            else
-            {
-                addTk(DIV);
-                pch++;
-            }
-            break;
-        }
-        case '.':
-        {
-            addTk(DOT);
-            pch++;
-            break;
-        }
-        case '&':
-        {
-            addTk(AND);
-            pch += 2;
-            break;
-        }
-        case '|':
-        {
-            addTk(OR);
-            pch += 2;
-            break;
-        }
-        case '!':
-        {
-            if (pch[1] == '=')
-            {
-                addTk(NOTEQ);
-                pch += 2;
-            }
-            else
-            {
-                addTk(NOT);
-                pch++;
-            }
-            break;
-        }
-        case '<':
-        {
-            if (pch[1] == '=')
-            {
-                addTk(LESSEQ);
-                pch += 2;
-            }
-            else
-            {
-                addTk(LESS);
-                pch++;
-            }
-            break;
-        }
-        case '>':
-        {
-            if (pch[1] == '=')
-            {
-                addTk(GREATEREQ);
-                pch += 2;
-            }
-            else
-            {
-                addTk(GREATER);
-                pch++;
-            }
-            break;
-        }
-
-        default:
-        {
-
-            
-            if (isalpha(*pch) || *pch == '_')
-            {
-                for (start = pch; isalnum(*pch) || *pch == '_'; pch++){}
-                char *text = extract(start, pch);
-
-                if (strcmp(text, "char") == 0)
-                    addTk(TYPE_CHAR);
-                else if (strcmp(text, "int") == 0)
-                    addTk(TYPE_INT);
-                else if (strcmp(text, "double") == 0)
-                    addTk(TYPE_DOUBLE);
-                else if (strcmp(text, "if") == 0)
-                    addTk(IF);
-                else if (strcmp(text, "else") == 0)
-                    addTk(ELSE);
-                else if (strcmp(text, "return") == 0)
-                    addTk(RETURN);
-                else if (strcmp(text, "struct") == 0)
-                    addTk(STRUCT);
-                else if (strcmp(text, "void") == 0)
-                    addTk(VOID);
-                else if (strcmp(text, "while") == 0)
-                    addTk(WHILE);
-                else
-                {
-		  tk = addTkText(ID, text);
-                    tk->text = text;
-                }
-            }
-            else
-            {
-	      for (start = pch; strchr(",;)([]{}",*pch)==0; pch++){printf("%c\n", *pch);}
-	       if(start!=pch)pch--;
-	      char *text = extract(start, pch);
-	      regexCheck(pch, text, tk);
-	       pch++;
-            }
-        }
-        }
-    }
-}
-
-void showTokens(const Token *tokens)
-{
-    const char *tokenStrings[] = {
-        "ID",
-
-        "TYPE_CHAR", "TYPE_DOUBLE", "TYPE_INT", "IF", "ELSE", "RETURN", "STRUCT", "VOID", "WHILE",
-
-        "COMMA", "SEMICOLON", "LPAR", "RPAR", "LBRACKET", "RBRACKET", "LACC", "RACC", "END",
-
-        "ASSIGN", "EQUAL", "ADD", "SUB", "MUL", "DIV", "DOT", "AND", "OR", "NOT", "NOTEQ", "LESS", "LESSEQ", "GREATER", "GREATEREQ",
-
-        "INT", "DOUBLE", "CHAR", "STRING"};
-    for (const Token *tk = tokens; tk; tk = tk->next)
-    {
-        printf("%d\t %s", tk->line, tokenStrings[tk->code]);
-	switch(tk->code){
-	case ID:printf(":%s\n", tk->text); break;
-	case INT: printf(":%d\n", tk->i); break;
-	case DOUBLE: printf(":%f\n", tk->d); break;
-	case STRING: printf(":%s\n", tk->text); break;
-	case CHAR: printf(":%c\n", tk->c); break;
-	default: printf("\n");
+Token *tokenize(const char *pch){
+  const char *start;
+  Token *tk;
+  for(;;){
+    switch(*pch){
+    case ' ':case '\t':pch++;break;
+    case '\r':		// handles different kinds of newlines (Windows: \r\n, Linux: \n, MacOS, OS X: \r or \n)
+      if(pch[1]=='\n')pch++;
+      // fallthrough to \n
+    case '\n':
+      line++;
+      pch++;
+      break;
+    case '\0':addTk(END);return tokens;
+    case ',':addTk(COMMA);pch++;break;
+    case '.':addTk(DOT);pch++;break;
+    case '*':addTk(MUL);pch++;break;
+    case '-':addTk(DIV);pch++;break;
+    case '+':addTk(ADD);pch++;break;
+    case ';':addTk(SEMICOLON);pch++;break;
+    case '(':addTk(LPAR);pch++;break;
+    case ')':addTk(RPAR);pch++;break;
+    case '[':addTk(LBRACKET);pch++;break;
+    case ']':addTk(RBRACKET);pch++;break;
+    case '{':addTk(LACC);pch++;break;
+    case '}':addTk(RACC);pch++;break;
+    case '/':
+      if(pch[1]=='/'){
+	while(*pch!='\n' && *pch!='\r' && *pch!='\0')pch++;
+      }else{
+	addTk(DIV);
+	pch++;
+      }
+      break;
+    case '!':
+      if(pch[1]=='='){
+	addTk(NOTEQ);
+	pch+=2;
+      }else{
+	addTk(NOT);
+	pch++;
+      }
+      break;
+    case '&':
+      if(pch[1]=='&'){
+	addTk(AND);
+	pch+=2;
+      }
+      else err("invalid char: %c (%d)",*pch,*pch);
+      break;
+    case '|':
+      if(pch[1]=='|'){
+	addTk(OR);
+	pch+=2;
+      }
+      else err("invalid char: %c (%d)",*pch,*pch);
+      break;
+    case '<':
+      if(pch[1]=='='){
+	addTk(LESSEQ);
+	pch+=2;
+      }else{
+	addTk(LESS);
+	pch++;
+      }
+      break;
+    case '>':
+      if(pch[1]=='='){
+	addTk(GREATEREQ);
+	pch+=2;
+      }else{
+	addTk(GREATER);
+	pch++;
+      }
+      break;
+    case '=':
+      if(pch[1]=='='){
+	addTk(EQUAL);
+	pch+=2;
+      }else{
+	addTk(ASSIGN);
+	pch++;
+      }
+      break;
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':{
+      char *number=(char*)safeAlloc(15);//salvam numarul intr-un string
+      int i=0;
+      while(isdigit(*pch) || *pch=='.' || *pch=='e' || *pch=='E' || *pch=='-'){ //poate fi int sau double
+	number[i]=*pch;
+	i++;
+	pch++;
+      }
+      number[i]='\0';
+      if(strchr(number, '.')||strchr(number, 'e')||strchr(number, 'E')) //daca e double
+	{
+	  double number_d = atof(number);
+	  tk=addTk(DOUBLE);
+	  tk->d=number_d;
 	}
+      else{ //daca e int
+	int nr = atoi(number);
+	tk=addTk(INT);
+	tk->i=nr;
+      }
+			    
+      break;
     }
+			
+    case '\'':
+      if(*(pch+2)=='\''){ //daca e char
+	tk=addTk(CHAR);
+	tk->c=*(pch+1);
+	pch+=2;
+      }
+      pch++;
+      break;
+			
+    case '\"':{
+      char *string=(char*)malloc(900*sizeof(char));
+      pch++;
+      int i;
+      for(i=0; *pch != '\"'; i++)
+	{
+	  string[i]=*pch; pch++;}
+      string[i]='\0';
+      tk=addTk(STRING);
+      tk->text=string;
+      pch++;
+      break;
+    }
+			
+    default:
+      if(isalpha(*pch)||*pch=='_'){ //daca e identificator
+	for(start=pch++;isalnum(*pch)||*pch=='_';pch++){}
+	char *text=extract(start,pch);
+	if(strcmp(text,"char")==0)addTk(TYPE_CHAR);
+	else if(strcmp(text,"double")==0)addTk(TYPE_DOUBLE);
+	else if(strcmp(text,"else")==0)addTk(ELSE);
+	else if(strcmp(text,"if")==0)addTk(IF);
+	else if(strcmp(text,"int")==0)addTk(TYPE_INT);
+	else if(strcmp(text,"return")==0)addTk(RETURN);
+	else if(strcmp(text,"struct")==0)addTk(STRUCT);
+	else if(strcmp(text,"void")==0)addTk(VOID);
+	else if(strcmp(text,"while")==0)addTk(WHILE);
+	else{
+	  tk=addTk(ID);
+	  tk->text=text;
+	}
+      }
+      else err("invalid char: %c (%d) at line %d",*pch,*pch, line);
+    }
+  }
+}
+
+void showTokens(const Token *tokens){
+  for(const Token *tk=tokens;tk;tk=tk->next){
+    printf("%d %s",tk->line, array[tk->code]);
+    if(tk->code == ID)
+      printf(":%s", tk->text);
+    else if(tk->code==INT)
+      printf(":%d", tk->i);
+    else if(tk->code==DOUBLE)
+      printf(":%f", tk->d);
+    else if(tk->code==CHAR)
+      printf(":%c", tk->c);
+    else if(tk->code==STRING)
+      printf(":%s", tk->text);
+    printf("\n");
+  }
 }
