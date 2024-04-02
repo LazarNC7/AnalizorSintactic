4f5459cc7fbcf0c5fed3283f5e447eb9da27b62f
LazarNC7 <109949877+LazarNC7@users.noreply.github.com>
2024-04-02 13:05:34 +0300

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,11 +1,28 @@
 #pragma once
-#include <stdbool.h>
+#include<stdbool.h>
 #include "lexer.h"
-#include "structDef.h"
-#include "fnDef.h"
-#include "varDef.h"
 
 void parse(Token *tokens);
-bool consume(int code);
-bool typeBase();
 bool unit();
+bool structDef();
+bool varDef();
+bool typeBase();
+bool arrayDecl();
+bool fnDef();
+bool fnParam();
+bool stm();
+bool stmCompound();
+bool expr();
+bool exprAssign();
+bool exprOr();
+bool exprAnd();
+bool expreEq();
+bool exprRel();
+bool exprEq();
+bool exprAdd();
+bool exprMul();
+bool exprCast();
+bool exprUnary();
+bool exorPostfix();
+bool exprPrimary();
+bool exprPostfix();
